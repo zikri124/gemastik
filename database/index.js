66364b27968c778cdbedd9f173f00c869f994b9e
zikri124 <faisalsaja73@gmail.com>
2020-09-07 23:53:25 +0700

    fix auth

--- database/index.js
+++ database/index.js
@@ -5,7 +5,8 @@ const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: 3306
 })
 
 db.query('select 1+1 as result',(err, result) =>{
